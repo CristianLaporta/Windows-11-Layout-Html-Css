3a6dc62de7bdddb8bc60c2e1290ddfff038da8fb
Cristian Laporta <37643992+TheCriicom@users.noreply.github.com>
2022-05-18 12:05:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Windows 11 Layout!</h1>
+<img src="immagini/7a.png" ><br>
+<h12>Calcolatrice</h2>
+<img src="immagini/1a.gif" ><br>
+<h12>Blocco Note</h2>
+<img src="immagini/2a.gif" ><br>
+<h12>Drag elements</h2>
+<img src="immagini/3a.gif" ><br>
+<h12>Change Background</h2>
+<img src="immagini/4a.gif" ><br>
+<h12>Browser</h2>
+<img src="immagini/5a.gif" ><br>
---
+++ assets/js/javascript.js
@@ -0,0 +1,111 @@
+function toggleExpand(id) {
+	let elem = document.getElementById(id)
+	/* toggle rimuove oppure inserisce una classe */
+	elem.classList.toggle("close")
+}
+/*  drag notepad e calcolatrice   */
+const position = {
+	x: 0,
+	y: 0
+}
+interact('.draggable').draggable({
+	listeners: {
+		start(event) {
+			console.log(event.type, event.target)
+		},
+		move(event) {
+			position.x += event.dx
+			position.y += event.dy
+
+			event.target.style.transform =
+				`translate(${position.x}px, ${position.y}px)`
+		},
+	}
+})
+/* cambia lo sfondo */
+
+let wallaper1 = document.querySelector('#wallaper-1');
+let wallaper2 = document.querySelector('#wallaper-2');
+let wallaper3 = document.querySelector('#wallaper-3');
+
+wallaper1.addEventListener('click', () => {
+	document.body.style.backgroundImage = "url('assets/img/sfondo.png')";
+});
+wallaper2.addEventListener('click', () => {
+	document.body.style.backgroundImage = "url('assets/img/sfondo2.png')";
+});
+wallaper3.addEventListener('click', () => {
+	document.body.style.backgroundImage = "url('assets/img/sfondo3.png')";
+});
+
+
+
+/* calcolatrice */
+let first
+let operation
+let second
+
+function pressNuber(n) {
+	/*   converto da numero a stringa */
+	n += ''
+	if (!operation) {
+		/* operatore ternario */
+		first = (!first) ? n : first + n
+		/*  cambio il testo al interno del id */
+		document.getElementById("history-value").innerText = first;
+		document.getElementById("output-value").innerText = "";
+	} else {
+		second = (!second) ? n : second + n
+
+		document.getElementById("history-value").innerText = second;
+	}
+}
+
+function pressOperation(op) {
+	if (!!second) pressTotal()
+
+	if (!!first) operation = op
+	document.getElementById("history-value").innerText += op;
+}
+
+function pressTotal() {
+	first = '' + execute(+first, operation, +second)
+	operation = null
+	second = null
+	document.getElementById("history-value").innerText = "";
+	document.getElementById("output-value").innerText = first;
+}
+
+function execute(a, op, b) {
+	switch (op) {
+		case '+':
+			return a + b
+		case '-':
+			return a - b
+		case '*':
+			return a * b
+		case '/':
+			return a / b
+	}
+	return 0
+}
+/*  cancella tutto */
+function erase() {
+	first = null
+	operation = null
+	second = null
+	document.getElementById("output-value").innerText = "0";
+	document.getElementById("history-value").innerText = "";
+
+}
+/* cancella solo il primo valore */
+function erase2() {
+
+	first = first.slice(0, -1)
+	document.getElementById("history-value").innerText = first;
+
+	document.getElementById("output-value").innerText = "";
+
+
+
+}   
